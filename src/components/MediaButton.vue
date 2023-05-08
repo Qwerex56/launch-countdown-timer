c1bcb1f11ba5266bdeec8bfdba041f7719145061
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-05-09 01:45:58 +0200

    Initial commit

---
+++ src/components/MediaButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <a
+    class="media-icon"
+    :href="mediaLink"
+  >
+    <img
+      class="media-icon__media-image" 
+      :src="icoImgSrc"
+      alt="mIco"
+    >
+  </a>
+</template>
+
+<script lang="ts">
+import icoDefault from '@/images/icon-facebook.svg';
+
+export default {
+  props: {
+    mediaLink: {
+      type: String,
+      required: false,
+      default: () => '#',
+    },
+    icoImgSrc: {
+      type: String,
+      default: () => icoDefault,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.media-icon {
+  
+  &__media-image {
+
+  }
+}
+</style>
